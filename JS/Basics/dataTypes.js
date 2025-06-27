89e119b164cd1188c87def46f7ed4b15c90317cb
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-06-27 16:59:04 +0530

    dataTypes

---
+++ JS/Basics/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) 
+let name="Akshita"//string
+let age=19
+let isFalse=false
+let state=null
+
+
+//primitive data types
+//number=> 2 to power 53
+//bigint -> in bid websites like facebook or stocks
+//string
+//boolean => false/true
+//null => standalone value
+//undefined=> when values is not assigned and null is a representation of empty value
+//null is a value that shows this variable is empty but it is defined 
+//symbol=> we will usethis while doing react , figma uses symbol a lot , for uniqueness it is used
+
+//object
+console.log(typeof " hi");//string in output 
+console.log(typeof age);//output: number
+console.log(typeof null)//output: object
+console.log(typeof undefined)//output: undefined
+
+
