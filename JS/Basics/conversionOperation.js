72ad44e0949ec181a815885aa36567996a520fef
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-06-29 19:39:17 +0530

    conversion operation

---
+++ JS/Basics/conversionOperation.js
@@ -0,0 +1,11 @@
+let score="33"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33"->33
+//"33abc"-> type: number, but NaN
+// true->1; false->0
+let name="Akshita"
+let repoCount=9
+console.log(`My name is ${name} and my repo count is ${repoCount}`);//String Interpolation
