1f603d011bf123292c1115765e848fd73d8627b0
Akshita-cyber <akshitatanwar2006@gmail.com>
2025-07-01 22:04:30 +0530

    jsEC&CallStack

---
+++ JS/03_Basics/JSExecution.js
@@ -0,0 +1,13 @@
+//when we give js a code, then a global execution context(ef) is made
+//this global ec is kept in a variable 'this'
+//interview- brower's and code enviromnet's global ec
+//browser's is important and it's global ec is window object
+//this works on hread
+//js is single threaded
+//1. global EC  2. Function EC  3. EVAL EC
+
+//1. Memory creation Phase  2. Execution phase
+
+
+
+//call stack
